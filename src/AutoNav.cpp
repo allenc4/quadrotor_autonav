1b0a7925e27cd6aa945eca8b1584a89fbb33e85d
Tyler Gauch <tyler.gauch@continuum.net>
2016-02-10 21:31:00 -0500

    updates to get map DOESN'T WORK

--- src/AutoNav.cpp
+++ src/AutoNav.cpp
@@ -1,10 +1,18 @@
 #include "AutoNav.h"
 
-AutoNav::AutoNav(ros::NodeHandle &nh)
-{
-	nh_ = nh;
-	cmd_vel_pub = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+nav_msgs::OccupancyGrid::ConstPtr map;
 
+void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr &msg)
+{
+	std::cout << "Got map";
+	map = msg;
+}
+
+AutoNav::AutoNav(ros::NodeHandle &n)
+{
+	nh = n;
+	cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+	map_sub = nh.subscribe("map", MAP_QUEUE_SIZE, mapCallback);
 }
 
 void AutoNav::sendMessage(float linX, float linY, float linZ, float angX, float angY, float angZ)
@@ -18,14 +26,20 @@ void AutoNav::sendMessage(float linX, float linY, float linZ, float angX, float 
 	cmd.angular.z = angZ;
 
 	cmd_vel_pub.publish(cmd);
-
 }
 
 void AutoNav::doNav(){
-	
-	while(true)
+	int i = 0;
+	while(i < 2)
 	{
+		std::cout << "Up" << std::endl;
 		sendMessage(0,0,0.5,0,0,0);
+		std::cout << "Sleep" << std::endl;
+		ros::Duration(2).sleep(); // sleep in seconds
+		std::cout << "Move" << std::endl;
+		sendMessage(0,-0.5,0,0,0,0);
+		std::cout << "Sleep" << std::endl;
+		ros::Duration(2).sleep(); // sleep in seconds
+		i++;
 	}
-
 }
--- src/AutoNav.h
+++ src/AutoNav.h
@@ -1,19 +1,24 @@
 #ifndef AUTO_NAV
 #define AUTO_NAV
 
+#define MAP_QUEUE_SIZE 10
+
 #include <ros/ros.h>
+#include <iostream>
+#include <unistd.h>
 #include <geometry_msgs/Twist.h>
+#include <nav_msgs/OccupancyGrid.h>
 
 class AutoNav{
 
 public:
-	AutoNav(ros::NodeHandle &nh);
+	AutoNav(ros::NodeHandle &n);
 	void doNav();
 	void sendMessage(float linX, float linY, float linZ, float angX, float angY, float angZ);
 private:
-	ros::NodeHandle nh_;
+	ros::NodeHandle nh;
 	ros::Publisher cmd_vel_pub;
-
+	ros::Subscriber map_sub;
 };
 
 #endif
